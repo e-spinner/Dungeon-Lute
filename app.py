938b2ea057a60ccc0e16e5670eef24a35b43bb73
e-spinner <e-spinner@onu.edu>
2024-06-27 11:37:19 -0400

    Now loads folders, local player done
    displays list of playlists in music directory, has a fade out when you selecect aspecific song or button, fixed some logical errors in case handeling.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 app = Flask( __name__ )
 
 # Paths to directories
-SOUNDS_PATH = os.path.join( app.root_path, 'static', 'sounds' )
+SOUNDS_PATH = os.path.join( app.root_path, 'music' )
 PRESETS_PATH = os.path.join( app.root_path, 'presets' )
 
 # Home Page
@@ -13,44 +13,41 @@ PRESETS_PATH = os.path.join( app.root_path, 'presets' )
 def index():
     return render_template( 'index.html' )
   
-# Request for buttons
-@app.route('/get_buttons')
+# Request for buttons.json
+@app.route( '/get_buttons' )
 def get_buttons():
     try:
-        with open(os.path.join(PRESETS_PATH, 'buttons.json'), 'r') as f:
-            buttons = json.load(f)
+        with open( os.path.join( PRESETS_PATH, 'buttons.json' ), 'r' ) as file:
+            buttons = json.load( file )
     except FileNotFoundError:
         buttons = []
-    return jsonify(buttons)  
+    return jsonify( buttons )  
 
-# Save Current Buttons
-@app.route('/save_buttons', methods=['POST'])
+# Save Current Buttons to buttons.json
+@app.route( '/save_buttons', methods=['POST'] )
 def save_buttons():
     buttons = request.json
-    with open(os.path.join(PRESETS_PATH, 'buttons.json'), 'w') as f:
-        json.dump(buttons, f)
-    return jsonify({"status": "success"})
+    with open( os.path.join( PRESETS_PATH, 'buttons.json' ), 'w' ) as file:
+        json.dump( buttons, file )
+    return jsonify( {"status": "success"} )
 
-@app.route('/upload', methods=['POST'])
-def upload_sound():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"})
+# find current playlists
+@app.route( '/playlists' )
+def find_playlists():
+    playlists = [dir for dir in os.listdir( SOUNDS_PATH ) if os.path.isdir( os.path.join( SOUNDS_PATH, dir ) )]
+    return jsonify( playlists )
     
-    file = request.files['file']
-    
-    if file.filename == '':
-        return jsonify({"error": "No selected file"})
-    
-    if file:
-        filepath = os.path.join(SOUNDS_PATH, file.filename)
-        file.save(filepath)
-        return jsonify({"filename": file.filename})
-    
-    return jsonify({"error": "File upload failed"})
+# retrive list of song in playlist
+@app.route( '/get_songs/<playlist>' )
+def get_songs( playlist ):
+    playlist_path = os.path.join( SOUNDS_PATH, playlist )
+    songs = [file for file in os.listdir( playlist_path ) if file.endswith( '.mp3' )]
+    return jsonify( songs )
 
-@app.route('/sounds/<filename>')
-def get_sound(filename):
-    return send_from_directory( SOUNDS_PATH, filename )
+# retrive song
+@app.route( '/sounds/<folder>/<filename>' )
+def get_sound( folder, filename ):
+    return send_from_directory( SOUNDS_PATH + '/' + folder, filename )
 
 if __name__ == '__main__':
     app.run( debug=True )
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener( 'DOMContentLoaded', ( event ) => {
     loadSoundboard();
-});
+} );
 
 let buttons = [];
 let activeButtonIndex = null;
@@ -8,37 +8,54 @@ let audio = null;
 let currentAudio = null;
 let lastPlayedSong = {};
 let history = [];
+let playlists = [];
+const FADE_OUT_DURATION = 1000; // Duration of fade out in milliseconds
 
 // Function to load the soundboard buttons
 function loadSoundboard() {
-    fetch('/get_buttons')
-        .then(response => response.json())
-        .then(data => {
+    fetch( '/get_buttons' )
+        .then( response => response.json() )
+        .then( data => {
             buttons = data;
             renderButtons();
-        });
+        } );
+
+    fetch( '/playlists' )
+    .then( response => response.json() )
+    .then( playlists => {
+        const playlistSelect = document.getElementById( 'playlist-select' );
+        playlists.forEach( playlist => {
+            const option = document.createElement( 'option' );
+            option.value = playlist;
+            option.innerText = playlist;
+            playlistSelect.appendChild( option );
+        } );
+    } );
 }
 
 // Function to add button-song combination to history
-function addToHistory(index, soundIndex) {
-    history.push({ index, soundIndex });
+function addToHistory( index, soundIndex ) {
+    history.push( { index, soundIndex } );
     updateHistoryDisplay();
 }
 
 // Function to update the history display
 function updateHistoryDisplay() {
-    const historyList = document.getElementById('historyList');
+    const historyList = document.getElementById( 'historyList' );
     historyList.innerHTML = '';
-    history.forEach((entry, index) => {
-        const listItem = document.createElement('li');
+    history.forEach( ( entry, index ) => {
+        const listItem = document.createElement( 'li' );
         listItem.innerText = `Step ${index + 1}: Button ${entry.index + 1} - Song ${entry.soundIndex + 1}`;
-        historyList.appendChild(listItem);
-    });
+        historyList.appendChild( listItem );
+    } );
 }
 
 // Function to rewind to previous button-song combination
 function rewindToPrevious() {
-    if (history.length > 1) {
+    if ( history.length > 1 ) {
+        let pause = document.getElementById( 'pause_play' );
+        pause.innerText = 'Pause';
+        
         history.pop(); // Remove the current button-song combination
         updateHistoryDisplay();
         const { index, soundIndex } = history[history.length - 1];
@@ -47,24 +64,25 @@ function rewindToPrevious() {
         stopCurrentAudio()
     
         activeButtonIndex = index;
-        playSound(index, soundIndex); 
-        showButtonDetails(index); 
+        playSound( index, soundIndex ); 
+        showButtonDetails( index ); 
 
         history.pop();
         updateHistoryDisplay();
+
     }
 }
 
 function next() {
     index = activeButtonIndex;
     stopCurrentAudio()
-    playRandomSound(index);
-    showButtonDetails(index);
+    playRandomSound( index );
+    showButtonDetails( index );
 }
 
 function pause_play() {
-    let button = document.getElementById('pause_play');
-    if (audio.paused) {
+    let button = document.getElementById( 'pause_play' );
+    if ( audio.paused ) {
         audio.play();
         button.innerText = 'Pause';
     }
@@ -76,21 +94,23 @@ function pause_play() {
 
 // Function to save the buttons
 function saveButtons() {
-    fetch('/save_buttons', {
+    fetch( '/save_buttons', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(buttons)
-    }).then(response => response.json())
-      .then(data => {
-          if (data.status === 'success') {
-              alert('Buttons saved successfully!');
+        body: JSON.stringify( buttons )
+    } ).then( response => response.json() )
+      .then( data => {
+          if ( data.status === 'success' ) {
+              alert( 'Buttons saved successfully!' );
           }
-      });
+      } );
 }
 
 function addButton() {
-    buttons.push({ name: '', sounds: [] });
-    renderButtons();
+    buttons.push( { name: '', sounds: [] } );
+    renderButtons();    
 }
+
+
