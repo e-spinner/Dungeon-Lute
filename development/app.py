02c41e7fd12c41c6bcec5869ee1a01eb16e02b95
e-spinner <e-spinner@onu.edu>
2025-02-03 11:57:11 -0500

    changing preset method
    calling presets decks, stoping the usage of local files, will store data in the application folder. That should be better, program currently broken, python is updated to new schema, js is not

--- development/app.py
+++ development/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, Response, render_template, jsonify, send_from_
 from pathlib import Path
 import sys
 from os import getenv, listdir, remove, kill, getpid
-from os.path import join, dirname, abspath, isdir
+from os.path import join, dirdeck, abspath, isdir
 from json import load, dump, JSONDecodeError
 from datetime import datetime
 from time import sleep
@@ -11,26 +11,13 @@ from signal import SIGINT
 
 app = Flask( __name__ )
 
-def get_local_path():
-    """Determine the local path based on the operating system."""
-    if sys.platform == 'win32':
-        path = Path( getenv( 'LOCALAPPDATA' ) ) / '.DungeonLute'
-    elif sys.platform == 'linux':
-        path = Path.home() / '.local' / 'share' / '.DungeonLute'
-    elif sys.platform == 'darwin':
-        path = Path.home() / 'Library' / 'Application Support' / '.DungeonLute'
-    else:
-        raise Exception( 'Unsupported OS' )
-    
-    return path
-
 def get_executable_path():
     """Get the path of the current executable or script."""
     if getattr( sys, 'frozen', False ):
         executable_path = sys.executable
     else:
         executable_path = __file__
-    executable_path = dirname( abspath( executable_path ) )
+    executable_path = dirdeck( abspath( executable_path ) )
     
     return executable_path
 
@@ -38,10 +25,7 @@ def get_executable_path():
 PLAYLISTS_PATH = join( get_executable_path(), 'playlists' )
 SOUNDS_PATH = join( get_executable_path(), 'sfx' )
 TRACKS_PATH = join( get_executable_path(), 'tracks' )
-PRESETS_PATH = join( get_local_path(), 'presets' )
-DATA_PATH = join( get_local_path(), 'data' )
-TEMPLATES_PATH = join( get_executable_path(), 'templates' )
-
+DECKS_PATH = join( get_executable_path(), 'data/decks' )
 
 @app.route( '/' )
 def index():
@@ -49,51 +33,71 @@ def index():
     s_log( 'index', 'Rendering main page' )
     return render_template( 'index.html' )
 
-# ======== #
-# MENU-BAR #
-# ======== #
+@app.route( '/edit' )
+def edit():
+    """Render the edit page."""
+    s_log( 'edit', 'Switching to edit page' )
+    return render_template( 'edit.html' )
+
+# =============== #
+# DECK MANAGEMENT #
+# =============== #
+
+@app.route( '/save/<deck>', methods=['POST'] )
+def save_deck( deck ):
+    """Save the current setup as a deck."""
+    data = request.json
+    sound_board = data['soundBoard']
+    css_rules = data['cssRules']
+    
+    with open( join( DECKS_PATH, str( deck ) + '.json' ), 'w' ) as file:
+        file.write( '' )
+        dump( sound_board, file )
+        
+    s_log( 'edit', f'deck {deck} saved' )
+    return jsonify( {'status': 'success'} )
 
 @app.route( '/load' )
-def get_presets():
-    """Return a list of available presets."""
-    presets = []
-    json_files = [f for f in listdir( PRESETS_PATH ) if f.endswith( '.json' )]
+def get_decks():
+    """Return a list of available decks."""
+    decks = []
+    json_files = [f for f in listdir( DECKS_PATH ) if f.endswith( '.json' )]
     
-    for preset in json_files:
-        name = preset[:-5]
-        presets.append( name )
+    for deck in json_files:
+        deck = deck[:-5]
+        decks.append( deck )
         
-    s_log( 'menu-bar', f'Presets loaded: {presets}' )
-    return jsonify( presets )
+    s_log( 'menu-bar', f'decks loaded: {decks}' )
+    return jsonify( decks )
 
-@app.route( '/load/<preset>' )
-def get_preset( preset ):
-    """Return the contents of a specific preset file."""
+@app.route( '/load/<deck>' )
+def get_deck( deck ):
+    """Return the contents of a specific deck file."""
     try:
-        with open( join( PRESETS_PATH, str( preset ) + '.json' ), 'r' ) as file:
+        with open( join( DECKS_PATH, str( deck ) + '.json' ), 'r' ) as file:
             playlists = load( file )
-        s_log( 'menu-bar', f'Preset {preset} loaded successfully' )
+        s_log( 'menu-bar', f'deck {deck} loaded successfully' )
         
     except FileNotFoundError:
         playlists = []
-        s_log( 'menu-bar', f'Preset {preset} not found' )
+        s_log( 'menu-bar', f'deck {deck} not found' )
         
     return jsonify( playlists )
 
-@app.route( '/del/<preset>' )
-def del_preset( preset ):
-    """Delete a specific preset file."""
+@app.route( '/del/<deck>' )
+def del_deck( deck ):
+    """Delete a specific deck file."""
     try:
-        remove( join( PRESETS_PATH, f'{preset}.json' ) )
-        s_log( 'menu-bar', f'Preset {preset} deleted successfully' )
-        return jsonify( {'message': 'Preset file deleted successfully'} ), 200
+        remove( join( DECKS_PATH, f'{deck}.json' ) )
+        s_log( 'menu-bar', f'deck {deck} deleted successfully' )
+        return jsonify( {'message': 'deck file deleted successfully'} ), 200
     
     except FileNotFoundError:
-        s_log( 'menu-bar', f'Preset {preset} not found for deletion' )
-        return jsonify( {'error': 'Preset file not found'} ), 404
+        s_log( 'menu-bar', f'deck {deck} not found for deletion' )
+        return jsonify( {'error': 'deck file not found'} ), 404
     
     except PermissionError:
-        s_log( 'menu-bar', f'Permission denied when deleting preset {preset}' )
+        s_log( 'menu-bar', f'Permission denied when deleting deck {deck}' )
         return jsonify( {'error': 'Permission denied'} ), 403
 
 # ========== #
@@ -173,79 +177,6 @@ def s_log( origin, message ):
     """Log server messages to the terminal."""
     timestamp = datetime.now().strftime( '%d/%b/%Y %H:%M:%S' )
     print( f'\033[31mPY-SERVER - - [{timestamp}][{origin.upper()}]: {message}\033[0m' )
-    
-    
-@app.route( '/save/data/<data>', methods=['POST'] )
-def save_data( data ):
-    """Save JSON data to a file."""
-    var = request.json
-    
-    with open( join( DATA_PATH, str( data ) + '.json' ), 'w' ) as file:
-        file.write( '' )
-        dump( var, file )
-        
-    s_log( 'utilities', f'Data saved to {data}.json' )
-    return jsonify( {'status': 'success'} )
-
-@app.route( '/load/data/<data>' )
-def load_data( data ):
-    """Load JSON data from a file."""
-    try:
-        with open( join( DATA_PATH, str( data ) + '.json' ), 'r' ) as file:
-            var = load( file )
-        s_log( 'utilities', f'Data loaded from {data}.json' )
-        
-    except FileNotFoundError:
-        var = []
-        s_log( 'utilities', f'Data file {data}.json not found' )
-        
-    except JSONDecodeError:
-        var = []
-        s_log( 'utilities', f'Error decoding JSON from file {data}.json' )
-        
-    return jsonify(var)
-
-# ==== #
-# EDIT #
-# ==== #
-
-@app.route( '/edit' )
-def edit():
-    """Render the edit page and clear the colors.css file."""
-    
-    with open( join( app.root_path, 'static', 'css', 'colors.css' ), 'w' ) as file:
-        file.write( '' )
-    s_log( 'edit', 'Switching to edit page' )
-    
-    return render_template( 'edit.html' )
-
-@app.route( '/save/<name>', methods=['POST'] )
-def save_preset( name ):
-    """Save the current setup as a preset."""
-    data = request.json
-    sound_board = data['soundBoard']
-    css_rules = data['cssRules']
-    
-    with open( join( PRESETS_PATH, str( name ) + '.json' ), 'w' ) as file:
-        file.write( '' )
-        dump( sound_board, file )
-        
-    with open( join( DATA_PATH, 'color.css' ), 'w' ) as file:
-        file.write( '' )
-        file.write( '\n'.join( css_rules ) )
-        
-    s_log( 'edit', f'Preset {name} saved' )
-    return jsonify( {'status': 'success'} )
-
-@app.route( '/data/colors.css' )
-def load_colors():
-    """Load and return the colors.css file."""
-    
-    with open( join( DATA_PATH, 'color.css' ), 'rb' ) as file:
-        css = file.read()
-        
-    s_log( 'edit', 'Colors.css loaded' )
-    return Response( css.decode( 'utf-8' ), content_type='text/css' )
 
 # ============== #
 # SERVER CONTROL #
@@ -272,30 +203,7 @@ def shutdown_server():
 
 if __name__ == '__main__':
     
-    path = get_local_path()
-    
-    s_log( 'driver', f'local path found: {path}' )
     s_log( 'driver', f'script path found: {get_executable_path()}' )
-
-    if not ( path / 'data' / 'color-default.json' ).exists():
-        # new installation
-        s_log( 'driver', 'new installation detected' )
-
-        data = path / 'data'
-        data.mkdir( parents=True, exist_ok=True )
-        
-        with open( data / 'color-default.json', 'w' ) as file:
-            dump( ['#ffffff', '#292b2c', '#343a40', '#007bff'], file )
-        with open( data / 'color.json', 'w' ) as file:
-            dump( ['#241f31', '#77767b', '#c0bfbc', '#f66151'], file )
-        with open( data / 'color.css', 'w' ) as file:
-            file.write( '' )
-
-        presets = path / 'presets'
-        presets.mkdir( parents=True, exist_ok=True )
-        
-        with open( presets / 'Default.json', 'w' ) as file:
-            file.write( '[[]]' )
-
+    
     s_log( 'driver', 'running flask app' )
     app.run( debug=True )
